e7ae30f27101d5913c1b0ddb94856634d85a7e15
Arialdo Martini <arialdomartini@gmail.com>
2015-10-07 22:48:15 +0200

    The (unformatted) form

--- src/index.php
+++ src/index.php
@@ -1,2 +1,10 @@
+<form action="" method="POST">
+  <label for="firsstname">First name</label>
+  <input name="firstname" />
+  <label for="secondname">Second name</label>
+  <input name="secondname" />
+  <label for="phonenumber">Phone number</label>
+  <input name="phonenumber" />
+  <input name="subscribe" type="submit" value="subscribe"/>
+</form>
 <?php
-echo "Hello, world!";
