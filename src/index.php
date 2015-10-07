517a587df9f9bb38cfddcddb3c7ac2d14a1bed6b
Arialdo Martini <arialdomartini@gmail.com>
2015-10-07 22:57:49 +0200

    A scaffold function for subscribing players

--- src/index.php
+++ src/index.php
@@ -8,3 +8,14 @@
   <input name="subscribe" type="submit" value="subscribe"/>
 </form>
 <?php
+   if($_POST["subscribe"]) {
+     $firstname = strip_tags($_POST["firstname"]);
+     $secondname = strip_tags($_POST["secondname"]);
+     $phonenumber = strip_tags($_POST["phonenumber"]);
+
+     subscribe($firstname, $secondname, $phonenumber);
+   }
+
+   function subscribe($firstname, $secondname, $phonenumber) {
+     echo "Subscribing $firstname";
+   }
