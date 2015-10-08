3208eddf7e0947e65a8ba530dba5fae9337fc131
Arialdo Martini <arialdomartini@gmail.com>
2015-10-08 09:13:02 +0200

    It persists players in a local file

--- src/index.php
+++ src/index.php
@@ -8,14 +8,70 @@
   <input name="subscribe" type="submit" value="subscribe"/>
 </form>
 <?php
-   if($_POST["subscribe"]) {
-     $firstname = strip_tags($_POST["firstname"]);
-     $secondname = strip_tags($_POST["secondname"]);
-     $phonenumber = strip_tags($_POST["phonenumber"]);
+define("PLAYERFILE", "/tmp/players.txt");
+//$initialPlayers = array(array("firstname" => "marta", "secondname" => "Lucci", "phonenumber" => "123"));
+if(!file_exists(PLAYERFILE))
+  saveplayers(array());
 
-     subscribe($firstname, $secondname, $phonenumber);
-   }
+$players = readplayers();
 
-   function subscribe($firstname, $secondname, $phonenumber) {
-     echo "Subscribing $firstname";
-   }
+
+if($_POST["subscribe"]) {
+  $firstname = strip_tags($_POST["firstname"]);
+  $secondname = strip_tags($_POST["secondname"]);
+  $phonenumber = strip_tags($_POST["phonenumber"]);
+
+  
+  $players = subscribe($players, $firstname, $secondname, $phonenumber);
+
+  saveplayers($players);
+}
+
+function subscribe($players, $firstname, $secondname, $phonenumber) {
+  $player = array(
+                  "firstname" => $firstname,
+                  "secondname" => $secondname,
+                  "phonenumber" => $phonenumber);
+  $players[] = $player;
+  return $players;
+}
+
+function readplayers() {
+  $content = file_get_contents(PLAYERFILE);
+  $decoded = json_decode($content, true);
+  return $decoded;
+}
+
+
+function saveplayers($players) {
+  file_put_contents(PLAYERFILE, json_encode($players));
+}
+
+
+
+
+
+?>
+<table>
+  <thead>
+   <tr>
+      <td>First name</td>
+      <td>Second name</td>
+      <td>Phone number</td>
+   </tr>
+
+  <thead>
+
+<?php
+foreach($players as $player):?>
+   <tr>
+      <td><?php echo $player["firstname"]?></td>
+      <td><?php echo $player["secondname"]?></td>
+      <td><?php echo $player["phonenumber"]?></td>
+   </tr>
+<?php
+endforeach;?>
+
+</table>
+<?php
+
