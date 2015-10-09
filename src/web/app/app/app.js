f643c34e77ca1b5d59900ba886b566534da53abc
lucax88x <lucax88x@gmail.com>
2015-10-09 18:36:25 +0200

    added validation

--- src/web/app/app/app.js
+++ src/web/app/app/app.js
@@ -1,7 +1,9 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
 var App;
 (function (App) {
-    angular.module('app', ['core'])
+    angular.module('app', ['core', 'validation', 'validation.rule'])
+        .config(['$validationProvider', function ($validationProvider) {
+        }])
         .config(["$provide", function ($provide) {
             $provide.decorator("$exceptionHandler", ["$delegate", function ($delegate) {
                     return function (exception, cause) {
--- src/web/src/app/app.ts
+++ src/web/src/app/app.ts
@@ -1,8 +1,10 @@
 ﻿/// <reference path="../../typings/angularjs/angular.d.ts" />
 
 module App {
-    angular.module('app', ['core'])
-        .config(["$provide", function($provide) {
+    angular.module('app', ['core', 'validation', 'validation.rule'])
+        .config(['$validationProvider', function($validationProvider) {            
+        }])
+        .config(["$provide", function($provide) {            
             $provide.decorator("$exceptionHandler", ["$delegate", function($delegate) {
                 return function(exception, cause) {
                     console.log(exception.message);
